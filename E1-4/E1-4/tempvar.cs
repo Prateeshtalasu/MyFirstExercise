a309511c081d82a3a7116d1e3dc27748795f033b
Prateeshtalasu <prateeshtalasu1999@gmail.com>
2020-07-13 18:13:12 +0530

    initial commit

---
+++ E1-4/E1-4/tempvar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E1_4
+{
+    class tempvar
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("entertwonum");
+            a = Convert.ToInt32(Console.ReadLine());
+            b= Convert.ToInt32(Console.ReadLine());
+            a = a - b;
+            b = a + b;
+            a = b - a;
+
+            Console.WriteLine("the number:" + a);
+            Console.WriteLine("the number:" + b);
+        }
+    }
+}
